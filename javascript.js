2cce202c4632fe6af0b441a41625050f791010cc
AmelaMusic <muam21gk@student.ju.se>
2022-11-24 11:17:59 +0100

    commit

--- javascript.js
+++ javascript.js
@@ -5,16 +5,29 @@ function clickaxe() {
 }
 
 function switchAxe() {
+  confirm(
+    "This isnt working, there are too many zombies, I need to find a way to get out of here! "
+  );
   document.getElementById("img1").style.display = "inline";
   document.getElementById("axeImage").style.display = "none";
 }
 
+// function clickstore() {
+//   var z = confirm("I need to find a way inside the store");
+//   if (z == true) {
+//     document.getElementById("lock").style.display = "inline";
+//     sessionStorage.setItem("lock", "key");
+//   } else {
+//   }
+// }
+
 function clickstore() {
-  var z = confirm("I need to find a way inside the store");
+  var z = confirm("Click on the lock to unlock the store!");
   if (z == true) {
     document.getElementById("lock").style.display = "inline";
     sessionStorage.setItem("lock", "key");
   } else {
+    document.getElementById("lock").style.display = "inline";
   }
 }
 
@@ -41,7 +54,9 @@ function codeLock() {
     document.getElementById("lock").style.display = "none";
     document.getElementById("img3").style.display = "none";
   } else if (c != 1010) {
-    var n = confirm("Sorry, Try again!");
+    var n = confirm(
+      "Wrong! I need to find the code, I should look in the woods"
+    );
     if (n == true) {
       codeLock();
     }
@@ -53,10 +68,15 @@ function codeLock() {
 var yes = document.getElementById("insidestore");
 
 function ClickKeys() {
-  confirm("Great, we found the keys!");
+  confirm(
+    "Great, we found the keys! Lets go back to the car and press on the image"
+  );
+
+  // document.getElementById("woodshint").style.display = "inline";
+
   sessionStorage.setItem("keys", "escape");
   confirm(
-    "The car is in a bad shape, I may need to try multiple times to start the car by pressing OK"
+    "P.S. The car is in a bad shape, You may need multiple tries to start the car by pressing OK"
   );
   document.getElementById("keys").style.display = "inline";
   document.getElementById("keysImage").style.display = "none";
@@ -85,14 +105,16 @@ function handleClicks() {
   document.getElementById("clicks").innerHTML = clicks;
   // if number of clicks is greater than one then show image 2
   if (clicks == 2) {
-    confirm("The car is locked but there is a note inside the car!");
+    confirm("The car is locked but there is a note inside the car! Pick it up");
     document.getElementById("img2").style.display = "inline";
     sessionStorage.setItem("note", "woods");
   } else if ("keys" in sessionStorage) {
     coinFlip();
   } else if (clicks == 1) {
     // if clicks aren't greater than 1 do something else
-    confirm("I can use this car to escape! Lets try and unlock it! ");
+    confirm(
+      "I can use this car to escape! Lets try and unlock it by pressing on the car again! "
+    );
   } else {
     return;
   }
@@ -107,7 +129,10 @@ function myFunction() {
     return;
   } else if (imgClicks == 1) {
     // if clicks aren't greater than 1 do something else
-    var r = confirm("There is an axe maybe I can use it to protect myself!");
+    var r = confirm(
+      "There is an axe on the image maybe I can use it to protect myself!"
+    );
+    confirm("Press the axe!");
   }
   if (r == true) {
   } else {
